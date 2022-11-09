35c0098ca46591d5edbecc70e6d4cce63e8e93af
Cofeek <94117043+cofeek-codes@users.noreply.github.com>
2022-11-09 14:53:57 +0500

    close file

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def main():
     words__file = open('words.txt')
     words = words__file.read().split('\n')
     output = f'{random.choice(words)} {random.choice(words)}'
+    words__file.close()
     print(output)
 
 
